0309c7f28fea482839aa3d91143069171082ad53
Sesay, Denny <denny.sesay@comline.de>
2024-09-23 12:27:56 +0200

    preliminary api endpoints

--- api.py
+++ api.py
@@ -1,7 +1,19 @@
 from flask import Blueprint, jsonify, request
 
-api_bp = Blueprint('api/v1', __name__)
+api = Blueprint('api/v1', __name__)
 
-@api_bp.route('/guess', methods=['GET'])
+@api.route('/guess', methods=['GET'])
 def check_guess():
     return 'test best'
+
+@api.route('/scoreboard', methods=['GET'])
+def get_scoreboard():
+    return 'score'
+
+@api.route('/name/<name>', method=['POST'])
+def set_name(name):
+    return name
+
+@api.route('/name/<id>', method=['GET'])
+def get_name(id):
+    return 'name'
